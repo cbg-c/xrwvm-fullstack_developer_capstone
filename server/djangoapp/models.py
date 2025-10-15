e2dfe3702e35942a7e045f58700a2fa4de35487d
cg-c <55558817+cbg-c@users.noreply.github.com>
2025-10-15 10:12:59 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,56 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Other fields as needed
+
+    def __str__(self):
+        return f"{self.name} - {self.description[:50]}..."  # Return the name as the string representation
+
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=100, blank=True, null=True)
+    founded_year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+# Car Model
+class CarModel(models.Model):
+    # Car type choices
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    HATCHBACK = 'Hatchback'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (HATCHBACK, 'Hatchback'),
+    ]
+
+    name = models.CharField(max_length=100)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='car_models')
+    dealer_id = models.IntegerField()  # Refers to dealer in Cloudant DB
+    car_type = models.CharField(max_length=20, choices=CAR_TYPES, default=SEDAN)
+    year = models.IntegerField()
+    color = models.CharField(max_length=50, blank=True, null=True)  # Optional extra field
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Optional extra field
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
+
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
